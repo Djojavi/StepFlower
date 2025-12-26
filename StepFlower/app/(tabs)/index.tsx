190f5d96feee6065afb62770fa367e37c01f6d08
Froggotkd <djojavi19@gmail.com>
2025-12-25 20:35:54 -0500

    project and 8bit font

---
+++ StepFlower/app/(tabs)/index.tsx
@@ -0,0 +1,42 @@
+import { Image } from 'expo-image';
+import { Text, StyleSheet, View, Pressable } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('@/assets/images/logo.png')}
+        style={styles.image}
+      />
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Comenzar</Text>
+      </Pressable>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2e7e5',
+    minHeight: '100%'
+  },
+  button:{
+    backgroundColor: '#ed7059',
+    padding:10,
+    borderRadius:15,
+    paddingHorizontal:50, 
+    marginTop:40
+  },
+  buttonText:{
+    fontSize: 15,
+    color:'#f2e7e5',
+    fontFamily: 'font8bit'
+  },
+  image: {
+    height: 320,
+    width: 320
+  }
+});
